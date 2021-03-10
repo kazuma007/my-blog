974768f2e96e00b3187ba38ae35cb4cc1e94f357
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-03-10 20:54:11 +0900

    added some settings to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
+/* eslint-disable */
 module.exports = {
   corePlugins: {
     listStyleType: false
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
   }
-};
+}
